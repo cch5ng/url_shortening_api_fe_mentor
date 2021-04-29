316d2b62a0804622206e850da1ece52ce8863c93
Carol Chung <cch5ng@gmail.com>
2021-04-29 11:22:39 -0700

    implement copy button logic

--- src/components/UrlCard.js
+++ src/components/UrlCard.js
@@ -2,15 +2,33 @@ import {useState} from 'react';
 
 function UrlCard({longUrl, shortUrl}) {
   const [isUrlCopied, setIsUrlCopied] = useState(false);
+  const [cardError, setCardError] = useState('');
+
+  const handleCopyClick = (ev) => {
+    ev.preventDefault();
+    navigator.permissions.query({name: "clipboard-write"}).then(result => {
+      if (result.state == "granted" || result.state == "prompt") {
+        if (navigator.clipboard) {
+          navigator.clipboard.writeText(shortUrl)
+            .then(function() {
+              setIsUrlCopied(true);
+            }, function() {
+              setIsUrlCopied(false);
+              setCardError('Cannot copy url to the clipboard. Please copy it manually.');
+            });  
+        }
+      }
+    });
+  }
 
   return (
     <div className="card-container flex flex-col justify-center items-start w-full 
       desktop:flex-row desktop:items-center">
       <div className="long_url w-full">{longUrl}</div>
-      <div className="flex flex-col desktop:flex-row desktop:items-center">
+      <div className="flex flex-col w-full desktop:flex-row desktop:items-center">
         <div className="short_url w-full">{shortUrl}</div>
         <div className="w-full">
-          <button className="btn btn_small">{isUrlCopied ? 'Copied!': 'Copy'}</button>
+          <button className="btn desktop:btn_small" onClick={handleCopyClick}>{isUrlCopied ? 'Copied!': 'Copy'}</button>
         </div>
       </div>
     </div>
