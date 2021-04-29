7fdde45caca919608245d77a1a244216c8a4f7bf
Carol Chung <cch5ng@gmail.com>
2021-04-29 10:37:57 -0700

    refactor url card for desktop layout

--- src/components/UrlCard.js
+++ src/components/UrlCard.js
@@ -4,11 +4,14 @@ function UrlCard({longUrl, shortUrl}) {
   const [isUrlCopied, setIsUrlCopied] = useState(false);
 
   return (
-    <div className="flex flex-col justify-center items-start w-full">
-      <div className="h-50px long_url w-full">{longUrl}</div>
-      <div className="h-50px short_url w-full">{shortUrl}</div>
-      <div className="w-full">
-        <button className="btn">{isUrlCopied ? 'Copied!': 'Copy'}</button>
+    <div className="card-container flex flex-col justify-center items-start w-full 
+      desktop:flex-row desktop:items-center">
+      <div className="long_url w-full">{longUrl}</div>
+      <div className="flex flex-col desktop:flex-row desktop:items-center">
+        <div className="short_url w-full">{shortUrl}</div>
+        <div className="w-full">
+          <button className="btn btn_small">{isUrlCopied ? 'Copied!': 'Copy'}</button>
+        </div>
       </div>
     </div>
   )
